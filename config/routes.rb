9f0ac47c4912cf9968b05ff19cd7b4b54d3f3c60
Gozi1 <snwabiani@gmail.com>
2023-03-21 03:17:13 +0000

    signin /login feature functioning

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   #   get 'categories/index'
   # end
   get "/about", to: "about#index"
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
