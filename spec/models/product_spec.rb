3b48403b10321693cb4293072e3d1b2fa7b9a459
Gozi1 <snwabiani@gmail.com>
2023-03-28 22:04:51 +0000

    product specs test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,14 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  describe 'Validations' do
-    # validation tests/examples here
-    #validates name
+    describe 'Validations' do
 
-    #validates price
-    
-    #validates quantitiy
-
-    #validates category
-  end
+      it 'should save when every criteria is meet' do
+        @category = Category.create(name: "Tested")
+        @product = Product.new
+        @product.name = 'test_Product'
+        @product.description = 'testing stuff'
+        @product.category = @category
+        @product.quantity = 5
+        @product.price = 5000
+        expect(@product.save).to be_truthy
+      end
+      it 'not save when name is missing ' do 
+        @category = Category.create(name: "Tested")
+        @product = Product.new
+        @product.description = 'testing stuff'
+        @product.category = @category
+        @product.quantity = 5
+        @product.price = 5000
+        expect(@product.save).to be_falsey
+      end
+      it 'not save when price is missing ' do 
+        @category = Category.create(name: "Tested")
+        @product = Product.new
+        @product.name = 'test_Product'
+        @product.description = 'testing stuff'
+        @product.category = @category
+        @product.quantity = 5
+        expect(@product.save).to be_falsey
+      end
+      it 'should not save when quantity is missing' do
+        @category = Category.create(name: "Tested")
+        @product = Product.new
+        @product.name = 'test_Product'
+        @product.description = 'testing stuff'
+        @product.category = @category
+        @product.price = 5000
+        expect(@product.save).to be_falsey
+      end
+      it 'should not save when catergory is missing' do
+        @product = Product.new
+        @product.name = 'test_Product'
+        @product.description = 'testing stuff'
+        @product.quantity = 5
+        @product.price = 5000
+        expect(@product.save).to be_falsey
+      end
+      
+    end
 end
