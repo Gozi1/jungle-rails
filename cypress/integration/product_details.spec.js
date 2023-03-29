7af488fd8f5ffdaf8fa7050141b7e350015caa83
Gozi1 <snwabiani@gmail.com>
2023-03-29 22:02:30 +0000

    test done

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,22 @@
+describe('example to-do app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.request('/cypress_rails_reset_state')
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  
+  it("There be a certain description on the page", () => {
+  cy.get(".products article").first().click().then(()=>{
+    cy.contain("The Giant Tea is an uncommon").should("be.visible")
+  });
+  
+
+});
+})
